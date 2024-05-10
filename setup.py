a2b49ece44de05f59a40cbee1c2cb6df9310788e
staraledreamer <168984783+staraledreamer@users.noreply.github.com>
2024-05-10 08:36:54 -0400

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,9 +1,18 @@
 from setuptools import setup, find_packages
 import codecs
 import os
+import importlib.util
 
 here = os.path.abspath(os.path.dirname(__file__))
 
+# Import finish.py
+finish_path = './finish.py'
+module_name = 'finish_module'
+spec = importlib.util.spec_from_file_location(module_name, finish_path)
+finish_module = importlib.util.module_from_spec(spec)
+spec.loader.exec_module(finish_module)
+
+# Read README.md for long description
 with codecs.open(os.path.join(here, "README.md"), encoding="utf-8") as fh:
     long_description = "\n" + fh.read()
 
@@ -32,3 +41,6 @@ setup(
         "Operating System :: Microsoft :: Windows",
     ]
 )
+
+# Call function from finish.py if needed
+finish_module.finish_setup()
